c33d8df2f1f2f985648b02fb1b3a3574dbafff2f
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-03-13 19:40:54 +0800

    Add multi-img manipulation, refactor codebase

--- pages/Quiz_1.py
+++ pages/Quiz_1.py
@@ -13,67 +13,90 @@ def translate(img, Tx, Ty, Bx_o, By_o):
         [1, 0, Bx_n],
         [0, 1, By_n],
         [0, 0, 1]])
-    translated_img = cv.warpPerspective(img, m_translation, (wdth,
-                                                             hght))
+    translated_img = cv.warpPerspective(img, m_translation, (wdth, hght))
     return translated_img
 
 
-# def translateImages(imgs):
-#     orig_img = cv.imread(imgs[0])
-#     cv.imshow('Original Image', orig_img)
-#     cv.waitKey(0)
-#     orig_img_wdth = orig_img.shape[1]
-#     orig_img_hght = orig_img.shape[0]
-#     comb_img = orig_img
-#     Bx = 20
-#     By = 50
-#     Tx = 60
-#     Ty = 70
-#     for i in range(len(imgs)):
-#         trgt_img = cv.imread(imgs[i])
-#         manip_img = translate(trgt_img, Bx, By, Tx, Ty)
-#         cv.imshow("Manipulated Image", manip_img)
-#         cv.waitKey(0)
-#         Bx += 25
-#         By += 25
-#         Tx += 25
-#         Ty += 25
-#         resized_img = cv.resize(manip_img,
-#                                 (orig_img_wdth, orig_img_hght),
-#                                 interpolation=cv.INTER_AREA)
-#         comb_img = np.concatenate((comb_img, resized_img), axis=1)
+def manipSingleImg():
+    x_o_px = 0
+    y_o_px = 0
 
-#     cv.imshow("Manipualted Images", comb_img)
-#     cv.waitKey(0)
+    col1, col2 = st.columns(2)
+    x_t_px = col1.number_input(
+        "Pixels to translate on X-Axis?", value=0, step=1)
+    y_t_px = col2.number_input(
+        "Pixels to translate on Y-Axis?", value=0, step=1)
+    img_file = st.file_uploader("Upload single image...")
+
+    if img_file and x_t_px is not None and y_t_px is not None:
+        img = Image.open(img_file)
+        np_img = np.array(img)
+        col1, col2 = st.columns(2)
+        col1.markdown("Image Width: **" +
+                      str(np_img.shape[1]) + "** px")
+        col2.markdown("Image Height: **" +
+                      str(np_img.shape[0]) + "** px")
+        manip_img = translate(np_img, x_t_px, y_t_px, x_o_px, y_o_px)
+        st.image(manip_img, channels="RGB")
+
+        x_o_px = x_t_px
+        y_o_px = y_t_px
+
+
+def manipMultiImg():
+    img_files = st.file_uploader(
+        "Upload multiple images...", accept_multiple_files=True)
+
+    t_step = st.number_input(
+        "Translation step?", value=0)
+    st.write("Translation step is added five times, resulting in five of the same images with compounding translation values.")
+    st.write("---")
+
+    cols1 = st.columns(2)
+    x_o_px = cols1[0].number_input("X-Origin in pixels?", value=0)
+    y_o_px = cols1[1].number_input("Y-Origin in pixels?", value=0)
+
+    cols2 = st.columns(2)
+    x_t_px = cols2[0].number_input(
+        "Pixels to translate on X-Axis?", value=0)
+    y_t_px = cols2[1].number_input(
+        "Pixels to translate on Y-Axis?", value=0)
+
+    if img_files is not None and t_step:
+        for i in range(len(img_files)):
+            img = Image.open(img_files[i])
+            np_img = np.array(img)
+            col1, col2 = st.columns(2)
+            col1.markdown("Image Width: **" +
+                          str(np_img.shape[1]) + "** px")
+            col2.markdown("Image Height: **" +
+                          str(np_img.shape[0]) + "** px")
+            cols = st.columns(5)
+
+            for j in range(5):
+                manip_img = translate(
+                    np_img, x_t_px, y_t_px, x_o_px, y_o_px)
+                cols[j].image(manip_img, channels="RGB")
+                x_o_px += t_step
+                y_o_px += t_step
+                x_t_px += t_step
+                y_t_px += t_step
+
+            x_o_px -= t_step * 5
+            y_o_px -= t_step * 5
+            x_t_px -= t_step * 5
+            y_t_px -= t_step * 5
 
 
 def main():
     st.write("# Quiz 1: Image Translation")
-    img_manip = st.selectbox("Manipulate single or multiple images", [
+    img_manip = st.selectbox("Manipulate single or multiple images?", [
                              "Single", "Multiple"])
 
-    x_o_px = 0
-    y_o_px = 0
-
     if img_manip == "Single":
-        col1, col2 = st.columns(2)
-        x_t_px = col1.number_input(
-            "Pixels to translate on X-Axis", value=0, step=1)
-        y_t_px = col2.number_input(
-            "Pixels to translate on Y-Axis", value=0, step=1)
-        img_file = st.file_uploader("Upload single image")
-
-        if img_file and x_t_px is not None and y_t_px is not None:
-            img = Image.open(img_file)
-            np_img = np.array(img)
-            manip_img = translate(np_img, x_t_px, y_t_px, x_o_px, y_o_px)
-            st.image(manip_img, channels="RGB")
-
-            x_o_px = x_t_px
-            y_o_px = y_t_px
-
+        manipSingleImg()
     else:
-        pass
+        manipMultiImg()
 
 
 if __name__ == "__main__":
